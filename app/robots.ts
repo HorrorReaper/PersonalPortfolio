9b19af13cd21053bac0c64813ef167be0ae59bf7
HorrorReaper <patrick.egi04@gmail.com>
2026-02-19 11:38:06 +0100

    robots update

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const site = process.env.NEXT_PUBLIC_SITE_URL || 'https://yourname.dev'
+const site = process.env.NEXT_PUBLIC_SITE_URL || 'https://patrickeger.vercel.app'
 
 export default function robots(): MetadataRoute.Robots {
   return {
